5b87c299f7f04429f5af1a2f97a6e6803b9b92f4
Cascade1080 <smccauley25@gmail.com>
2022-04-09 11:51:50 -0400

    practice with git

--- beer_reader.py
+++ beer_reader.py
@@ -11,12 +11,15 @@ df_sixtyfour = df.iloc[68:81, 0:2]
 df_halfs = df.iloc[82:105]
 
 #applies a particular function to the selected row
-df_pints["bbls"] = df_pints["Num Sold"]*0.00403226.round(df_pints["bbls"],2)
+df_pints["bbls"] = df_pints["Num Sold"]*0.00403226
 df_mug["bbls"] = df_mug["Num Sold"]*0.00554435
 df_PTR["bbls"] = df_PTR["Num Sold"]*0.01612903
 df_thirtytwo["bbls"] = df_thirtytwo["Num Sold"]*0.00806452
 df_sixtyfour["bbls"] = df_sixtyfour["Num Sold"]*0.01612903
 df_halfs["bbls"] = df_halfs["Num Sold"]*0.00201613
+
+#sums the data and creates a new column displaying the total
+
 
 #prints the data
 print(df_pints)
