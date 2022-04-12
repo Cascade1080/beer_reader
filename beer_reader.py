ec0f28430bdb955ee12fb2f09550cc36f4d9cba9
Cascade1080 <smccauley25@gmail.com>
2022-04-12 15:58:38 -0400

    added 'total' and 'totaltwo', created totals_df dataframe

--- beer_reader.py
+++ beer_reader.py
@@ -1,46 +1,54 @@
 import pandas as pd
+pd.options.mode.chained_assignment = None #removes 'SettingWithCopyWarning' message
+
 
 df = pd.read_csv('january taproom sales 2022.csv', usecols=['Item Name','Num Sold'], nrows = 104)
 
 #Selects the range by row
-df_pints = df.iloc[0:22,0:2]
-df_mug = df.iloc[23:41,0:2]
-df_PTR = df.iloc[42:50, 0:2]
-df_thirtytwo = df.iloc[52:67, 0:2]
-df_sixtyfour = df.iloc[68:81, 0:2]
-df_halfs = df.iloc[82:105]
+df_pints = df.iloc[0:21,0:2]
+df_mug = df.iloc[22:40,0:2]
+df_PTR = df.iloc[41:49, 0:2]
+df_thirtytwo = df.iloc[52:66, 0:2]
+df_sixtyfour = df.iloc[68:80, 0:2]
+df_halfs = df.iloc[81:105]
 
 #applies a particular function to the selected row
-df_pints["bbls"] = df_pints["Num Sold"]*0.00403226
-df_mug["bbls"] = df_mug["Num Sold"]*0.00554435
-df_PTR["bbls"] = df_PTR["Num Sold"]*0.01612903
-df_thirtytwo["bbls"] = df_thirtytwo["Num Sold"]*0.00806452
-df_sixtyfour["bbls"] = df_sixtyfour["Num Sold"]*0.01612903
-df_halfs["bbls"] = df_halfs["Num Sold"]*0.00201613
+df_pints["BBLS"] = df_pints["Num Sold"]*0.00403226
+df_mug["BBLS"] = df_mug["Num Sold"]*0.00554435
+df_PTR["BBLS"] = df_PTR["Num Sold"]*0.01612903
+df_thirtytwo["BBLS"] = df_thirtytwo["Num Sold"]*0.00806452
+df_sixtyfour["BBLS"] = df_sixtyfour["Num Sold"]*0.01612903
+df_halfs["BBLS"] = df_halfs["Num Sold"]*0.00201613
 
-#sums the data and creates a new column displaying the total
+#sums the bbls column
+total = df_pints.sum(axis = 0, skipna = True) + df_mug.sum(axis = 0, skipna = True) + df_PTR.sum(axis = 0, skipna = True)
+totaltwo = df_thirtytwo.sum(axis = 0, skipna = True) + df_sixtyfour.sum(axis = 0, skipna = True) + df_halfs.sum(axis = 0, skipna = True)
+
+pints = df_pints.sum(axis = 0, skipna = True)
+
+totals = {'Item': ['Pints'], 'Total BBLS':[pints]}
+totals_df = pd.DataFrame(totals)
+
+#--------------------PRINTS THE LIST OF THE ITEM
+#print(df_pints)
+#print(df_mug)
+#print(df_PTR)
+#print(df_thirtytwo)
+#print(df_sixtyfour)
+#print(df_halfs)
+
+#--------------------PRINTS THE SUM OF THE ITEM
+#print(df_pints.sum(axis = 0, skipna = True))
+#print(df_mug.sum(axis = 0, skipna = True))
+#print(df_PTR.sum(axis = 0, skipna = True))
+#print(df_thirtytwo.sum(axis = 0, skipna = True))
+#print(df_sixtyfour.sum(axis = 0, skipna = True))
+#print(df_halfs.sum(axis = 0, skipna = True))
+print(total + totaltwo)
+#print(pints)
+#print(totals_df)
 
 
-#prints the data
-print(df_pints)
-print(df_mug)
-print(df_PTR)
-print(df_thirtytwo)
-print(df_sixtyfour)
-print(df_halfs)
-#df["BBLs"] = df["Num Sold"]*0.00403226 #selects and creates a new column with the wanted math expression
-
-#splits = [df.loc[[i]] for i in df.index] #splits dataframe by row
 
 
-
-###### selects specfic set of rows
-#print(df.iloc[0:21])
-#print(df.iloc[23:40])
-#print(df.iloc[42:49])
-
-#to_bbls = df[['Num Sold']] - selects the specific column
-
-#print(df)
-#print(to_bbls)
 print(df["Num Sold"].shape)
