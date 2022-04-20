914fbd63eaf2a91a35c2dd87f33359b503b2878a
Cascade1080 <smccauley25@gmail.com>
2022-04-20 17:40:24 -0400

    removed empty lines from csv

--- beer_reader.py
+++ beer_reader.py
@@ -3,6 +3,7 @@ pd.options.mode.chained_assignment = None #removes 'SettingWithCopyWarning' mess
 
 
 df = pd.read_csv('january taproom sales 2022.csv', usecols=['Item Name','Num Sold'], nrows = 104)
+df = df.dropna()
 
 #Selects the range by row
 df_pints = df.iloc[0:21,0:2]
@@ -10,7 +11,7 @@ df_mug = df.iloc[22:40,0:2]
 df_PTR = df.iloc[41:49, 0:2]
 df_thirtytwo = df.iloc[52:66, 0:2]
 df_sixtyfour = df.iloc[68:80, 0:2]
-df_halfs = df.iloc[81:105]
+df_halfs = df.iloc[81:104, 0:2]
 
 #applies a particular function to the selected row
 df_pints["BBLS"] = df_pints["Num Sold"]*0.00403226
@@ -30,7 +31,7 @@ totals = {'Item': ['Pints'], 'Total BBLS':[pints]}
 totals_df = pd.DataFrame(totals)
 
 #--------------------PRINTS THE LIST OF THE ITEM
-#print(df_pints)
+print(df_pints)
 #print(df_mug)
 #print(df_PTR)
 #print(df_thirtytwo)
@@ -38,17 +39,23 @@ totals_df = pd.DataFrame(totals)
 #print(df_halfs)
 
 #--------------------PRINTS THE SUM OF THE ITEM
-#print(df_pints.sum(axis = 0, skipna = True))
+print(df_pints.sum(axis = 0, skipna = True))
 #print(df_mug.sum(axis = 0, skipna = True))
 #print(df_PTR.sum(axis = 0, skipna = True))
 #print(df_thirtytwo.sum(axis = 0, skipna = True))
 #print(df_sixtyfour.sum(axis = 0, skipna = True))
 #print(df_halfs.sum(axis = 0, skipna = True))
-print(total + totaltwo)
+#print(total + totaltwo)
+
+
 #print(pints)
-#print(totals_df)
+print(totals_df)
 
 
 
 
 print(df["Num Sold"].shape)
+
+# Notes -
+# 'in'
+# drop.na
